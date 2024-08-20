2150e9a45079601fe5ee325a78e7948507f196db
alex28sh <alex.shefer.31@gmail.com>
2024-08-20 22:47:30 +0200

    src

---
+++ src/nagini_translation/lib/jvmaccess.py
@@ -0,0 +1,32 @@
+"""
+Copyright (c) 2019 ETH Zurich
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+import jpype
+
+
+class JVM:
+    """
+    Encapsulates access to a JVM
+    """
+
+    def __init__(self, classpath: str):
+        jpype.startJVM(jpype.getDefaultJVMPath(),
+                       '-Djava.class.path=' + classpath, '-Xss32m',
+                       convertStrings=True)
+        self.java = jpype.JPackage('java')
+        self.scala = jpype.JPackage('scala')
+        self.viper = jpype.JPackage('viper')
+        self.fastparse = jpype.JPackage('fastparse')
+
+    def get_proxy(self, supertype, instance):
+        return jpype.JProxy(supertype, inst=instance)
+
+    def get_array(self, t, n):
+        return jpype.JArray(t)(n)
+
+    def is_known_class(self, class_object) -> bool:
+        return not isinstance(class_object, jpype.JPackage)
