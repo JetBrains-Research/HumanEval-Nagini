129ab750162df35bed6d6e45e9ce02d4662d48bc
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-01-11 11:28:18 +0100

    fix 34, 40, 104

--- Bench/040-triples-sum-to-zero.py
+++ Bench/040-triples-sum-to-zero.py
@@ -35,11 +35,11 @@ def triples__sum__to__zero(l : List[int]) -> bool:
             Invariant(((i) >= (0)) and ((i) < (len(l))))
             Invariant(((j) >= (0)) and ((j) <= (len(l))))
             Invariant(Forall(int, lambda i1:
-                Forall(int, lambda j:
+                Forall(int, lambda j1:
                     Forall(int, lambda k:
-                        (Implies(((((((((0) <= (i1)) and ((i1) < (i))) and (((0) <= (j)) and ((j) < (len(l))))) and (((0) <= (k)) and ((k) < (len(l)))))) or 
-                                (i1 == i and (j) >= 0 and (j) < j and (k) >= 0 and (k) < len(l)))
-                                and ((i1) != (j)) and ((j) != (k))) and ((i1) != (k)), ((((l)[i1]) + ((l)[j])) + ((l)[k])) != (0)), [[(((l)[i1]) + ((l)[j])) + ((l)[k])]])))))
+                        (Implies(((((((((0) <= (i1)) and ((i1) < (i))) and (((0) <= (j1)) and ((j1) < (len(l))))) and (((0) <= (k)) and ((k) < (len(l)))))) or
+                                (i1 == i and (j1) >= 0 and (j1) < j and (k) >= 0 and (k) < len(l)))
+                                and ((i1) != (j1)) and ((j1) != (k))) and ((i1) != (k)), ((((l)[i1]) + ((l)[j1])) + ((l)[k])) != (0)), [[(((l)[i1]) + ((l)[j1])) + ((l)[k])]])))))
             # invariants-end
             k : int = 0
             while (k) < (len(l)):
@@ -49,12 +49,12 @@ def triples__sum__to__zero(l : List[int]) -> bool:
                 Invariant(((j) >= (0)) and ((j) < (len(l))))
                 Invariant(((k) >= (0)) and ((k) <= (len(l))))
                 Invariant(Forall(int, lambda i1:
-                    Forall(int, lambda j:
-                        Forall(int, lambda k:
-                            (Implies(((((((((0) <= (i1)) and ((i1) < (i))) and (((0) <= (j)) and ((j) < (len(l))))) and (((0) <= (k)) and ((k) < (len(l)))))) or 
-                                (i1 == i and (j) >= 0 and (j) < j and (k) >= 0 and (k) < len(l)) or 
-                                (i1 == i and (j) == j and (k) >= 0 and (k) < k))
-                                and ((i1) != (j)) and ((j) != (k))) and ((i1) != (k)), ((((l)[i1]) + ((l)[j])) + ((l)[k])) != (0)), [[(((l)[i1]) + ((l)[j])) + ((l)[k])]])))))
+                    Forall(int, lambda j1:
+                        Forall(int, lambda k1:
+                            (Implies(((((((((0) <= (i1)) and ((i1) < (i))) and (((0) <= (j1)) and ((j1) < (len(l))))) and (((0) <= (k1)) and ((k1) < (len(l)))))) or
+                                (i1 == i and (j1) >= 0 and (j1) < j and (k1) >= 0 and (k1) < len(l)) or
+                                (i1 == i and (j1) == j and (k1) >= 0 and (k1) < k))
+                                and ((i1) != (j1)) and ((j1) != (k1))) and ((i1) != (k1)), ((((l)[i1]) + ((l)[j1])) + ((l)[k1])) != (0)), [[(((l)[i1]) + ((l)[j1])) + ((l)[k1])]])))))
                 # invariants-end
                 if ((((i) != (j)) and ((j) != (k))) and ((i) != (k))) and (((((l)[i]) + ((l)[j])) + ((l)[k])) == (0)):
                     return True
